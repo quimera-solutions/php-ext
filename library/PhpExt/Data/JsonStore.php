9051a89972f664e31c50383be1d2060c2458379a
sergeiw <sergeiw@e0da71ca-fc45-0410-af39-5f59eeb7a8d0>
2008-05-19 01:07:46 +0000

    JsonReader properties added.  The reason for this is that according to Ext JS documentation, JsonStore inherits all config properties from JsonReader for an easier configuration.

--- library/PhpExt/Data/JsonStore.php
+++ library/PhpExt/Data/JsonStore.php
@@ -80,6 +80,88 @@ class PhpExt_Data_JsonStore extends PhpExt_Data_Store
 	}
     
 	
+	/************* JsonReader Properties ******************/
+	/**#@+
+	 * JsonReader Config Options:
+	 * 
+	 * This class inherits all of the config options of Store, JsonReader.
+	 * 
+	 */
+	// Id
+    /**
+     * Name of the property within a row object that contains a record identifier value.
+     * @param string $value
+     * @return PhpExt_Data_JsonReader
+     */
+    public function setId($value) {
+    	$this->setExtConfigProperty("id", $value);
+    	return $this;
+    }	
+    /**
+     * Name of the property within a row object that contains a record identifier value.
+     * @return string
+     */
+    public function getId() {
+    	return $this->getExtConfigProperty("id");
+    }
+    
+    // Root
+    /**
+     * Name of the property which contains the Array of row objects.
+     * @param string $value
+     * @return PhpExt_Data_JsonReader
+     */
+    public function setRoot($value) {
+    	$this->setExtConfigProperty("root", $value);
+    	return $this;
+    }	
+    /**
+     * Name of the property which contains the Array of row objects.
+     * @return string
+     */
+    public function getRoot() {
+    	return $this->getExtConfigProperty("root");
+    }
+    
+    // SuccessProperty
+    /**
+     * Name of the property from which to retrieve the success attribute used by forms.
+     * @param string $value
+     * @return PhpExt_Data_JsonReader
+     */
+    public function setSuccessProperty($value) {
+    	$this->setExtConfigProperty("successProperty", $value);
+    	return $this;
+    }	
+    /**
+     * Name of the property from which to retrieve the success attribute used by forms.
+     * @return string
+     */
+    public function getSuccessProperty() {
+    	return $this->getExtConfigProperty("successProperty");
+    }
+    
+    // TotalProperty
+    /**
+     * Name of the property from which to retrieve the total number of records in the dataset. This is only needed if the whole dataset is not passed in one go, but is being paged from the remote server.
+     * @param string $value
+     * @return PhpExt_Data_JsonReader
+     */
+    public function setTotalProperty($value) {
+    	$this->setExtConfigProperty("totalProperty", $value);
+    	return $this;
+    }	
+    /**
+     * Name of the property from which to retrieve the total number of records in the dataset. This is only needed if the whole dataset is not passed in one go, but is being paged from the remote server.
+     * @return string
+     */
+    public function getTotal() {
+    	return $this->getExtConfigProperty("totalProperty");
+    }	
+	/**#@-*/
+	
+	
+	
 	public function __construct() {
 		parent::__construct();
 		$this->setExtClassInfo("Ext.data.JsonStore", null);
@@ -87,7 +169,12 @@ class PhpExt_Data_JsonStore extends PhpExt_Data_Store
 		$validProps = array(
 		    "data",
 		    "fields",
-		    "url"
+		    "url",
+		    // JsonReader
+		    "id",
+		    "root",
+		    "successProperty",
+		    "totalProperty"
 		);
 		$this->addValidConfigProperties($validProps);
 
@@ -100,7 +187,7 @@ class PhpExt_Data_JsonStore extends PhpExt_Data_Store
 	 * @return PhpExt_Data_FieldConfigObject
 	 */
 	public function addField($field) {
-		$this->_fields->add($field, $field->Name);
+		$this->_fields->add($field, $field->getName());
 		return $field;
 	}
 	
